0c431528a12ab8fcec983de133d6b2e73f9d4f28
wangyingjie <wangyingjie@wftech.com.cn>
2023-09-20 11:23:15 +0800

    修改资源位置

--- music-website-server/src/main/java/com/example/salieri/constant/constant.java
+++ music-website-server/src/main/java/com/example/salieri/constant/constant.java
@@ -3,5 +3,5 @@ package com.example.salieri.constant;
 public class constant {
     public static final String RESOURCE_PATH_WIN = "D:\\Resource";
 
-    public static final String RESOURCE_PATH_MAC = "/Resource";
+    public static final String RESOURCE_PATH_MAC = "/root/Projects/Resource";
 }
